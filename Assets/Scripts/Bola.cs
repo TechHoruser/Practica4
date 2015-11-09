e3b55df71bed08d0e4c7f515c9b894900db8009c
mortalswat <pon_ton_ce@hotmail.com>
2015-11-09 22:41:49 +0100

    ZigZag Final

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Bola : MonoBehaviour
+{
+    bool derecha;
+    bool pausa;
+    float velocidad;
+
+    // Use this for initialization
+    void Start () {
+        velocidad = 0.1f;
+        derecha = true;
+        pausa = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Juego Acabado
+        if (this.transform.position.y <= 0)
+        {
+
+            GameObject ob = GameObject.Find("Terreno");
+            Main m = (Main)ob.GetComponent(typeof(Main));
+            if (!pausa)
+            {
+                pausa = true;
+            }
+
+            m.botoncillo.SetActive(true);
+
+            ob = GameObject.Find("Canvas");
+            Guardado guardar = ob.GetComponent<Guardado>();
+            guardar.mostrarLista();
+
+            Time.timeScale = 0;
+        }
+        //En otro caso
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                //if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
+                derecha = !derecha;
+
+            if (derecha)
+                this.transform.Translate(new Vector3(velocidad, 0f, 0f));
+            else
+                this.transform.Translate(new Vector3(0f, 0f, velocidad));
+        }
+    }
+
+    public void incrementarVelocidad()
+    {
+        velocidad += 0.01f;
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.transform.parent.gameObject.name == "Obstaculo")
+        {
+            Destroy(c.gameObject);
+            GameObject ob = GameObject.Find("Terreno");
+            Main m = (Main)ob.GetComponent(typeof(Main));
+            m.incrementarPuntuacion();
+        }
+    }
+}
---
+++ Assets/Scripts/Guardado.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using LitJson;
+using System.IO;
+
+public class Guardado : MonoBehaviour
+{
+    private const int tam = 5;
+    private jugador[] top5 = new jugador[tam];
+    private string[] jsontext;
+    private JsonData jsonData;
+
+    public void mostrarLista()
+    {
+        fCargarJSON();
+        //Muestra la lista
+        GameObject ob = GameObject.Find("Text");
+        Text t = ob.GetComponent<Text>();
+        ob = GameObject.Find("Terreno");
+        Main m = (Main)ob.GetComponent(typeof(Main));
+
+        t.text = ("Puntuacion: " + m.puntuacion() + "\nTiempo: " + m.tiempo());
+
+        for (int i = 0; top5[i]!=null; i++)
+        {
+            t.text += ("\n" + (i+1) + " " + top5[i].nombre + top5[i].puntuacion + top5[i].tiempo);
+        }
+
+        //Actualiza la lista
+        int pos = 0;
+
+        while (top5[pos]!=null && m.puntuacion() < top5[pos].puntuacion && pos<5)
+        {
+            pos++;
+        }
+        if(pos!=0)
+            pos++;
+
+        if (pos < 5)
+        {
+            for (int i = tam - 2; i >= pos; i--)
+                top5[i + 1] = top5[i];
+
+            ob = GameObject.Find("Text3");
+            Text input = ob.GetComponent<Text>();
+            ob = GameObject.Find("Terreno");
+            m = (Main)ob.GetComponent(typeof(Main));
+            top5[pos].nombre = input.text;
+            top5[pos].puntuacion = m.puntuacion();
+            top5[pos].tiempo = m.tiempo();
+
+            /*InputField input = gameObject.GetComponent<InputField>();
+            InputField.SubmitEvent se = new InputField.SubmitEvent();
+            se.AddListener(SubmitName);
+            input.onEndEdit = se;*/
+            //fGrabarJSON();
+        }
+    }
+    /*
+    private void SubmitName(string texto)
+    {
+        GameObject ob = GameObject.Find("Terreno");
+        Main m = (Main)ob.GetComponent(typeof(Main));
+        top5[pos].nombre = texto;
+        top5[pos].puntuacion = m.puntuacion();
+        top5[pos].tiempo = m.tiempo();
+    }*/
+
+    private void fCargarJSON()
+    {
+        if (!File.Exists(Application.dataPath + "/Player.json"))
+            File.Create(Application.dataPath + "/Player.json");
+
+        jsontext = File.ReadAllLines(Application.dataPath + "/Player.json");
+        for (int i = 0; i < jsontext.Length; i++)
+        {
+            jsonData = JsonMapper.ToObject(jsontext[i]);
+            top5[i].nombre = jsonData["nombre"].ToString();
+            top5[i].puntuacion = int.Parse(jsonData["puntuacion"].ToString());
+            top5[i].tiempo = float.Parse(jsonData["tiempo"].ToString());
+        }
+    }
+
+    private void fGrabarJSON()
+    {
+        JsonData playerJSON;
+        FileStream fs;
+
+        if (!File.Exists(Application.dataPath + "/Player.json"))
+            File.Create(Application.dataPath + "/Player.json");
+
+        fs = File.OpenWrite(Application.dataPath + "/Player.json");
+
+        StreamWriter sw = new StreamWriter(fs);
+
+        for (int i = 0; i < tam-1; i++)
+        {
+            playerJSON = JsonMapper.ToJson(top5[i]);
+            sw.Write(playerJSON.ToString()+"\n");
+        }
+        playerJSON = JsonMapper.ToJson(top5[4]);
+        sw.Close();
+        fs.Close();
+    }
+
+    private class jugador
+    {
+        public string nombre;
+        public int puntuacion;
+        public float tiempo;
+
+        public jugador(string nombre, int puntuacion, float tiempo)
+        {
+            this.nombre = nombre;
+            this.puntuacion = puntuacion;
+            this.tiempo = tiempo;
+        }
+
+    }
+}
